7bc126d01b9b8701c7636c7dbc5a64159962b7a7
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-03-10 20:42:07 -0500

    Author buildFilterCategoryOptions() and buildDiscover() functions
    Explicitly call buildDiscover() on script load; e.g., on 'discover' page load.

--- assignment/midterm/resources/js/build-content.js
+++ assignment/midterm/resources/js/build-content.js
@@ -30,6 +30,31 @@ const AUTHOR_BLOB = '950254564695883776';
 const CATEGORY_BLOB = '951604363324047360';
 
 /**
+ * Build the dynamic discover page content: filter menu options.
+ */
+async function buildDiscover() {
+	await buildFilterCategoryOptions();
+}
+
+/**
+ * Retrieve the available categories from the 'category' JSON document and parse each value as an option of the
+ * 'category' selection dropdown in the filter menu.
+ */
+async function buildFilterCategoryOptions() {
+	let data = await retrieve(CATEGORY_BLOB);
+	let list = document.getElementById('filter-category');
+
+	data.categories.forEach(category => {
+		let option = document.createElement('option');
+
+		option.setAttribute('value', category);
+		option.innerText = category;
+
+		list.append(option);
+	});
+}
+
+/**
  * Create an HTTP request at the {@link GET_ENDPOINT} with the provided JSON Blob ID to retrieve the data that the
  * corresponding document contains and parse it as JSON.
  *
@@ -47,3 +72,5 @@ async function retrieve(blobId) {
 		console.log(error);
 	}
 }
+
+buildDiscover().then(r => console.log('d'));
