67fd9fc14958732a1329c1759ea21cf9422927cc
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-04-01 13:35:33 -0400

    Include HTTP verb boilerplate

--- assignment/assignment-5/src/index.js
+++ assignment/assignment-5/src/index.js
@@ -7,3 +7,19 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const port = 3000;
+
+app.post('/api/jsonBlob', (request, response) => {
+    // fill in post request here
+});
+
+app.get('/api/jsonBlob/:blobId', (request, response) => {
+    // fill in get request here
+});
+
+app.put('/api/jsonBlob/:blobId', (request, response) => {
+    // fill in put request here
+});
+
+app.delete('/api/jsonBlob/:blobId', (request, response) => {
+    // fill in delete request here
+});
