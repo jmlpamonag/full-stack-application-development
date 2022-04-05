5789bac8ee956bcf36cb1cb3124817a7660ec672
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-04-05 12:37:31 -0400

    Author POST route to write unique JSON file
    A JSON file is created based on the validated request body and, if successful, the URL including the generated unique identifier is returned in the response header.

--- assignment/assignment-5/src/index.js
+++ assignment/assignment-5/src/index.js
@@ -7,18 +7,31 @@ const uuid = require('uuid');
 
 const port = 3000;
 
+const publicDirectory = `${__dirname}/../public`;
+
 app.use(express.json({
-    verify: (request, response, buffer, encoding) => {
+    verify: (request, response, buffer) => {
         try {
-            JSON.parse(encoding);
+            JSON.parse(buffer);
         } catch (error) {
             response.status(406).send('JSON could not be successfully validated - please try again!');
         }
     }
 }));
 
+app.use(bodyParser.json());
+
 app.post('/api', (request, response) => {
-    // fill in post request here - responsibility: michael
+    let id = uuid.v4();
+
+    fs.writeFile(`${publicDirectory}/${id}.json`, JSON.stringify(request.body, null, 4), (error) => {
+        if (error) {
+            response.status(500).send('An error was caught while writing the JSON file - please try again!');
+        }
+
+        response.location(`localhost:3000/api/${id}`);
+        response.status(201).send('A JSON document has successfully been generated and stored.')
+    });
 });
 
 app.get('/api/:id', (request, response) => {
