f3d0663556e2f0f264b0b7da38ba4fe1e5ed0e13
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-04-05 02:48:10 -0400

    Revise uuid package requirement from module import statement

--- assignment/assignment-5/src/index.js
+++ assignment/assignment-5/src/index.js
@@ -1,10 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
-
 const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const uuid = require('uuid');
 
 const port = 3000;
 
